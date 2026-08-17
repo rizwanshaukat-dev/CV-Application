358d0643ad2c87639d7d9f5cf9901d0193feae44
Rizwan-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-08-18 00:42:42 +0500

    Add edit/submit button logic to the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import GeneralInfo from './components/GeneralInfo';
 function App() {
-  const [generalInfo,setGeneralInfo]=useState({
-    firstName:"",
-    lastName:"",
-    email:"",
-    phone:""
+  const [generalInfo, setGeneralInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: ""
+  });
+  const [education, setEducation] = useState({
+    schoolName: "",
+    titleOfStudy: "",
+    dateOfStudy: ""
   });
   return (
     <>
-    <GeneralInfo generalInfo={generalInfo} setGeneralInfo={setGeneralInfo}/>
+      <GeneralInfo generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
     </>
   )
 }
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,55 +1,79 @@
-export default function GeneralInfoForm({generalInfo,setGeneralInfo}){
+import { useState } from "react"
+
+export default function GeneralInfo({ generalInfo, setGeneralInfo }) {
+    const [isEditing, setIsEditing] = useState(true);
+    function handleSubmitButton() {
+        setIsEditing(false)
+    }
+    function handleEditButton() {
+        setIsEditing(true)
+    }
     function handleFirstNameChange(event) {
         setGeneralInfo({
             ...generalInfo,
-            firstName:event.target.value
+            firstName: event.target.value
         })
     }
     function handleLastNameChange(event) {
         setGeneralInfo({
             ...generalInfo,
-            lastName:event.target.value
+            lastName: event.target.value
         })
     }
     function handleEmailChange(event) {
         setGeneralInfo({
             ...generalInfo,
-            email:event.target.value
+            email: event.target.value
         })
     }
     function handlePhoneChange(event) {
         setGeneralInfo({
             ...generalInfo,
-            phone:event.target.value
+            phone: event.target.value
         })
     }
-    return(
-        <div className="general-component">
+    if (isEditing) {
+        return (
+            <div className="general-edit-component">
+                <h2 className="title">General Information</h2>
+                <label htmlFor="first-name">First Name:</label>
+                <input type="text"
+                    id="first-name"
+                    value={generalInfo.firstName}
+                    onChange={handleFirstNameChange}
+                />
+                <label htmlFor="last-name">Last Name:</label>
+                <input type="text"
+                    id="last-name"
+                    value={generalInfo.lastName}
+                    onChange={handleLastNameChange}
+                />
+                <label htmlFor="email">Email:</label>
+                <input type="email"
+                    id="email"
+                    value={generalInfo.email}
+                    onChange={handleEmailChange}
+                />
+                <label htmlFor="phone">Phone Number:</label>
+                <input type="tel"
+                    id="phone"
+                    value={generalInfo.phone}
+                    onChange={handlePhoneChange}
+                />
+                <button className="submit-btn" onClick={handleSubmitButton}>Submit</button>
+            </div>
+        )
+    }
+    return (
+        <div className="general-display-component">
             <h2 className="title">General Information</h2>
-            <label htmlFor="first-name">First Name:</label>
-        <input type="text" 
-        id="first-name"
-        value={generalInfo.firstName}
-        onChange={handleFirstNameChange}
-        />
-        <label htmlFor="last-name">Last Name:</label>
-        <input type="text" 
-        id="last-name"
-        value={generalInfo.lastName}
-        onChange={handleLastNameChange}
-        />
-        <label htmlFor="email">Email:</label>
-        <input type="email" 
-        id="email"
-        value={generalInfo.email}
-        onChange={handleEmailChange}
-        />
-        <label htmlFor="phone">Phone Number:</label>
-        <input type="tel" 
-        id="phone"
-        value={generalInfo.phone}
-        onChange={handlePhoneChange}
-        />
+            <h3>Full Name:</h3>
+            <h4>{generalInfo.firstName} {generalInfo.lastName}</h4>
+            <h3>Email:</h3>
+            <h4>{generalInfo.email}</h4>
+            <h3>Phone Number:</h3>
+            <h4>{generalInfo.phone}</h4>
+            <button className="edit-btn" onClick={handleEditButton}>Edit</button>
         </div>
     )
 }
